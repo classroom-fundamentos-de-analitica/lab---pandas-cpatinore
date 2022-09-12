3c237837c9081d6dd68d44408ad6da1223c268a3
cpatinore <cpatinore@unal.edu.co>
2022-09-12 15:23:47 -0500

    Intento 4.2

--- preguntas.py
+++ preguntas.py
@@ -229,8 +229,9 @@ def pregunta_12():
     X= tbl2
     X= X.sort_values(by=['_c5a'])
     X["_c5"]= X["_c5a"] +":"+ X["_c5b"].astype(str)
-    
-    return X.groupby(["_c0"], as_index=False)['_c5'].agg(lambda x: ','.join(x.astype(str)))
+    X=X.groupby(["_c0"], as_index=False)['_c5'].agg(lambda x: ','.join(x.astype(str)))
+    return X
+
 
 def pregunta_13():
     """
